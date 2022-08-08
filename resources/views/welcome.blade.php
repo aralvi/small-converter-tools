892ec2101429e92477e138f9aa9c7ffea5507d96
unknown <aralvi143@gmail.com>
2022-08-09 00:46:57 +0500

    home-page-update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,9 +20,9 @@
     </head>
 
     <body class="antialiased">
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar sticky-top navbar-expand-lg bg-light">
             <div class="container">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="{{route('/')}}">
                     <img src="{{ asset('assets/icons/logo.svg') }}" alt="" width="300" height="24" class="d-inline-block align-text-top" />
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -81,11 +81,12 @@
                 <p class="text-center">We have converted <b class="cl-537895">1,545,407,398</b> with a total size of <b class="cl-537895">30,750</b> TB</p>
                 <h2 class="text-center fw-bold mt-5">Converter tools</h2>
                 <div class="row justify-content-around my-5">
-                    <div class="card bg-537895 text-light w-15 my-2 cursor-pointer stretched-link" data-target="#converToPDF">
+                    <div class="card bg-537895 text-light w-15 my-2">
                         <div class="card-body text-center">
                             <h5 class="card-title fw-bold">Convert To PDF</h5>
                             <img src="{{asset('assets/icons/convert-to-pdf.svg')}}" alt="" srcset="" />
                             <p class="card-text">Easily convert your PDF files into easy to edit DOC and DOCX documents. The converted document is almost 100% accurate.</p>
+                            <a href="#convertToPDF" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="card bg-537895 text-light w-15 my-2">
@@ -93,6 +94,7 @@
                             <h5 class="card-title fw-bold">Convert From PDF</h5>
                             <img src="{{asset('assets/icons/convert-from-pdf.svg')}}" alt="" srcset="" />
                             <p class="card-text">Easily convert your PDF files into easy to edit DOC and DOCX documents. The converted document is almost 100% accurate.</p>
+                            <a href="#convertFromPDF" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="card bg-537895 text-light w-15 my-2">
@@ -100,6 +102,7 @@
                             <h5 class="card-title fw-bold">Convert To Image</h5>
                             <img src="{{asset('assets/icons/convert-to-img.svg')}}" alt="" srcset="" />
                             <p class="card-text">Easily convert your PDF files into easy to edit DOC and DOCX documents. The converted document is almost 100% accurate.</p>
+                            <a href="#convertToImage" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="card bg-537895 text-light w-15 my-2">
@@ -107,6 +110,7 @@
                             <h5 class="card-title fw-bold">Convert From Image</h5>
                             <img src="{{asset('assets/icons/convert-from-img.svg')}}" alt="" srcset="" />
                             <p class="card-text">Easily convert your PDF files into easy to edit DOC and DOCX documents. The converted document is almost 100% accurate.</p>
+                            <a href="#convertFromImage" class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
@@ -115,8 +119,8 @@
 
         <div class="container">
             {{-- /* ----------------------------- convert to pdf start ----------------------------- */ --}}
-            <h2 class="text-center fw-bold">Convert To PDF</h2>
-            <div class="row d-flex justify-content-around" id="converToPDF">
+            <h2 class="text-center fw-bold mt-5" id="convertToPDF">Convert To PDF</h2>
+            <div class="row d-flex justify-content-around" >
                 <div class="card bg-F9FDFF border border-info w-18 my-3">
                     <div class="card-body">
                         <h5 class="card-title fw-bold">Word to PDF</h5>
@@ -193,8 +197,8 @@
             </div>
             {{-- /* ----------------------------- convert to pdf ends ----------------------------- */ --}} {{-- /* ---------------------------- convert from pdf start ---------------------------- */ --}}
 
-            <h2 class="text-center fw-bold">Convert From PDF</h2>
-            <div class="row d-flex justify-content-around" id="converFromPDF">
+            <h2 class="text-center fw-bold mt-5" id="convertFromPDF">Convert From PDF</h2>
+            <div class="row d-flex justify-content-around" >
                 <div class="card bg-F9FDFF border border-info w-18 my-3">
                     <div class="card-body">
                         <h5 class="card-title fw-bold">PDF to Word</h5>
@@ -241,8 +245,8 @@
                 </div>
             </div>
             {{-- /* -------------------------- convert from pdf ends ------------------------- */ --}} {{-- /* ---------------------------- convert to image start ---------------------------- */ --}}
-            <h2 class="text-center fw-bold">Convert To Image</h2>
-            <div class="row d-flex justify-content-around" id="convertToImage">
+            <h2 class="text-center fw-bold mt-5" id="convertToImage">Convert To Image</h2>
+            <div class="row d-flex justify-content-around" >
                 <div class="card bg-FCF6FF border border-info w-18 my-3">
                     <div class="card-body">
                         <h5 class="card-title fw-bold">PDF to JPG</h5>
@@ -274,8 +278,8 @@
                 </div>
             </div>
             {{-- /* -------------------------- convert to image ends ------------------------- */ --}} {{-- /* ---------------------------- convert from image start ---------------------------- */ --}}
-            <h2 class="text-center fw-bold">Convert From Image</h2>
-            <div class="row d-flex justify-content-around" id="convertFromImage">
+            <h2 class="text-center fw-bold mt-5" id="convertFromImage">Convert From Image</h2>
+            <div class="row d-flex justify-content-around" >
                 <div class="card bg-FCF6FF border border-info w-18 my-3">
                     <div class="card-body">
                         <h5 class="card-title fw-bold">JPG to PDF</h5>
@@ -307,41 +311,44 @@
                 </div>
             </div>
             {{-- /* -------------------------- convert to image ends ------------------------- */ --}}
-            <div class="row text-center bg-light my-5">
-                <h2 class="my-3 fw0vold">Why Choose Small Convertrer Tools</h2>
-                <div class="col-md-4 col-sm-6 mb-md-4">
-                    <img src="{{asset('assets/icons/bulb.svg')}}" class="mb-3" alt="icon" srcset="">
-                    <p class="fw-bolder">Quick and easy conversion</p>
-                    <p>With just a simple drag-and-drop, you can convert text or image to any format within seconds. There’s no file size limit nor even the need to register to use our service.</p>
-                </div>
-                <div class="col-md-4 col-sm-6 mb-md-4">
-                    <img src="{{asset('assets/icons/badge.svg')}}" class="mb-3" alt="icon" srcset="">
-                    <p class="fw-bolder">We keep your files safe!</p>
-                    <p>We care about privacy. All files will be deleted from our servers forever after one hour. To know more about how much we care, read our privacy policy.</p>
-                </div>
-                <div class="col-md-4 col-sm-6 mb-md-4">
-                    <img src="{{asset('assets/icons/thumb.svg')}}" class="mb-3" alt="icon" srcset="">
-                    <p class="fw-bolder">All platforms are welcome!</p>
-                    <p>Our small converter works on all computers - no matter if you use Mac, Windows or Linux.</p>
-                </div>
-                <div class="col-md-4 col-sm-6 mb-md-4">
-                    <img src="{{asset('assets/icons/lock.svg')}}" class="mb-3" alt="icon" srcset="">
-                    <p class="fw-bolder">PDF to image in the best quality</p>
-                    <p>PDF to image conversion is hard. To bring you the very best quality to convert your PDF files, we have partnered with Solid Documents - the best solution provider on the market.</p>
-                </div>
-                <div class="col-md-4 col-sm-6 mb-md-4">
-                    <img src="{{asset('assets/icons/converter.svg')}}" class="mb-3" alt="icon" srcset="">
-                    <p class="fw-bolder">Convert a PDF to a DOC in seconds</p>
-                    <p>It is seriously ridiculously easy with our tool to convert files online. Try it and you will love it.</p>
-                </div>
-                <div class="col-md-4 col-sm-6 mb-md-4">
-                    <img src="{{asset('assets/icons/tree.svg')}}" class="mb-3" alt="icon" srcset="">
-                    <p class="fw-bolder">Magic conversion in the cloud</p>
-                    <p>We have many servers in the cloud which do nothing else than converting PDF to Image files or text. So, lean back and let them do the work.</p>
+        </div>
+        <section class="bg-light">
+            <div class="container">
+                <div class="row text-center bg-light my-5">
+                    <h2 class="my-3 fw0vold">Why Choose Small Convertrer Tools</h2>
+                    <div class="col-md-4 col-sm-6 mb-md-4">
+                        <img src="{{asset('assets/icons/bulb.svg')}}" class="mb-3" alt="icon" srcset="">
+                        <p class="fw-bolder">Quick and easy conversion</p>
+                        <p>With just a simple drag-and-drop, you can convert text or image to any format within seconds. There’s no file size limit nor even the need to register to use our service.</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 mb-md-4">
+                        <img src="{{asset('assets/icons/badge.svg')}}" class="mb-3" alt="icon" srcset="">
+                        <p class="fw-bolder">We keep your files safe!</p>
+                        <p>We care about privacy. All files will be deleted from our servers forever after one hour. To know more about how much we care, read our privacy policy.</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 mb-md-4">
+                        <img src="{{asset('assets/icons/thumb.svg')}}" class="mb-3" alt="icon" srcset="">
+                        <p class="fw-bolder">All platforms are welcome!</p>
+                        <p>Our small converter works on all computers - no matter if you use Mac, Windows or Linux.</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 mb-md-4">
+                        <img src="{{asset('assets/icons/lock.svg')}}" class="mb-3" alt="icon" srcset="">
+                        <p class="fw-bolder">PDF to image in the best quality</p>
+                        <p>PDF to image conversion is hard. To bring you the very best quality to convert your PDF files, we have partnered with Solid Documents - the best solution provider on the market.</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 mb-md-4">
+                        <img src="{{asset('assets/icons/converter.svg')}}" class="mb-3" alt="icon" srcset="">
+                        <p class="fw-bolder">Convert a PDF to a DOC in seconds</p>
+                        <p>It is seriously ridiculously easy with our tool to convert files online. Try it and you will love it.</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 mb-md-4">
+                        <img src="{{asset('assets/icons/tree.svg')}}" class="mb-3" alt="icon" srcset="">
+                        <p class="fw-bolder">Magic conversion in the cloud</p>
+                        <p>We have many servers in the cloud which do nothing else than converting PDF to Image files or text. So, lean back and let them do the work.</p>
+                    </div>
                 </div>
             </div>
-        </div>
-
+        </section>
         <script src="{{ asset('assets/bootstrap-5.2.0-dist/js/bootstrap.min.js') }}"></script>
     </body>
 </html>
