8525a005ac4c5ec062c0b638d7cc849c0dc281c4
unknown <aralvi143@gmail.com>
2022-08-09 23:40:53 +0500

    favicon-svg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <title>{{env('APP_NAME')}}</title>
-        <link rel="shortcut icon" href="{{asset('assets/icons/favicon.png')}}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{asset('assets/icons/favicon.svg')}}" type="image/x-icon">
         <link rel="stylesheet" href="{{ asset('assets/bootstrap-5.2.0-dist/css/bootstrap.min.css') }}" />
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" />
