9e2201bfd94db22c83ea853159b5b472b3bdfaf2
unknown <aralvi143@gmail.com>
2022-08-10 00:51:41 +0500

    testominal-cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,7 +122,7 @@
             </div>
         </header>
 
-        <div class="container">
+        <div class="container" id="toolsToConvert">
             {{-- /* ----------------------------- convert to pdf start ----------------------------- */ --}}
             <h2 class="text-center fw-bold mt-5" id="convertToPDF">Convert To PDF</h2>
             <div class="row d-flex justify-content-around" >
@@ -366,9 +366,33 @@
             <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, illo hic. Aperiam voluptatem </p>
             <p class="text-center text-muted"><b class="fw-bold text-warning">4.9</b> out of 5  — <small>23 Ratings</small></p>
             <div class="bg-537895 position-relative rounded" style="height: 400px;">
-                <div class=" position-absolute rounded-circle bg-537895 p-3 top-30 left-50 border border-white">
+                <div class="position-absolute rounded-circle bg-537895 p-3 top-30 left-50 border border-white">
                     <img src="{{asset('assets/icons/quote.svg')}}" width="50px" alt="">
 
+                </div>
+                <div class="ml-135 d-flex">
+                    <div class="card bg-F9FDFF border border-info w-18 my-3">
+                        <div class="card-body">
+                            <small class="bg-537895 p-2 rounded-circle">
+                                <img class="card-title" src="{{asset('assets/icons/quote.svg')}}" width="20px" alt="" srcset="">
+                            </small>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eum laboriosam ratione eos consectetur esse ad odit at iste, velit dolorum? Corrupti, necessitatibus perspiciatis. Laboriosam qui necessitatibus temporibus magnam consequuntur.
+                            </p>
+                            <p class="fw-bolder mb-0">Nancy Roy</p>
+                            <div class="d-flex justify-content-between align-items-center text-muted">
+                                <div class="d-flex">
+                                    @for ($i = 0; $i < 3; $i++)
+                                    <img src="{{asset('assets/icons/star-yellow.svg')}}" width="15px" alt="" srcset="">
+                                    @endfor
+                                    <img src="{{asset('assets/icons/star-grey.svg')}}" width="15px" alt="" srcset="">
+                                    <img src="{{asset('assets/icons/star-grey.svg')}}" width="15px" alt="" srcset="">
+                                </div>
+                                <p class="mb-0">{{\Carbon\Carbon::now()->toFormattedDateString()}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
