d2557019933c6418e71818b9c94949c07c4a8453
unknown <aralvi143@gmail.com>
2022-08-17 02:08:29 +0500

    upload-file

--- resources/views/upload-file.blade.php
+++ resources/views/upload-file.blade.php
@@ -2,18 +2,26 @@
 
 @section('content')
 <div class="jumbotron bg-light">
-    <h2 class="text-capitalize text-center"><img src="{{asset('assets/icons/word-to-pdf.svg')}}" width="35px" alt="" srcset="">{{str_replace('-',' ',$name)}}</h2>
+    <h2 class="text-capitalize text-center"><img src="{{asset('assets/icons/word-to-pdf.svg')}}" width="35px" alt="" srcset=""> {{str_replace('-',' ',$name)}}</h2>
         <p class="text-center">Quick and easy way to convert your PDF to <br> editable Word document</p>
-        <div class="row mx-0">
-            <button class="btn btn-large btn-info">text</button>
-            <button class="btn btn-large btn-success">text</button>
-            <button class="btn btn-large btn-danger">text</button>
-
+        <div class="d-flex flex-column">
+            <div class="w-25 m-auto">
+                <label for="files" class="btn border form-control bg-537895 text-white"><img src="{{asset('assets/icons/desktop.svg')}}" alt="" srcset=""> Choose File</label>
+                <input id="files" class="form-control" style="visibility:hidden;" type="file">
+            </div>
+            <div class="w-25 m-auto">
+                <label for="files" class="btn border form-control"><img src="{{asset('assets/icons/dropbox.svg')}}" alt="" srcset=""> Dropbox</label>
+                <input id="files" class="form-control" style="visibility:hidden;" type="file">
+            </div>
+            <div class="w-25 m-auto">
+                <label for="files" class="btn border form-control"><img src="{{asset('assets/icons/drive.svg')}}" alt="" srcset=""> Google Drive</label>
+                <input id="files" class="form-control" style="visibility:hidden;" type="file">
+            </div>
         </div>
 </div>
 
 
-<div class="container">
+<div class="container my-5">
     <div class="row">
         <h4 class="text-center">How To Convert A PDF To Word Online:</h4>
         <div class="col-lg-12 m-auto d-flex justify-content-evenly align-items-center">
