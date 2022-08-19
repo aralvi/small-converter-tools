50e53daa492c2adfd50de7abdcd553f2d40d1f81
unknown <aralvi143@gmail.com>
2022-08-19 12:18:00 +0500

    upload-file-updated

--- resources/views/upload-file.blade.php
+++ resources/views/upload-file.blade.php
@@ -5,15 +5,15 @@
     <h2 class="text-capitalize text-center"><img src="{{asset('assets/icons/word-to-pdf.svg')}}" width="35px" alt="" srcset=""> {{str_replace('-',' ',$name)}}</h2>
         <p class="text-center">Quick and easy way to convert your PDF to <br> editable Word document</p>
         <div class="d-flex flex-column">
-            <div class="w-25 m-auto col-sm-12">
+            <div class=" m-auto col-sm-12 col-md-6 col-lg-3">
                 <label for="files" class="btn border form-control bg-537895 text-white"><img src="{{asset('assets/icons/desktop.svg')}}" alt="" srcset=""> Choose File</label>
                 <input id="files" class="form-control" style="visibility:hidden;" type="file">
             </div>
-            <div class="w-25 m-auto col-sm-12">
+            <div class=" m-auto col-sm-12 col-md-6 col-lg-3">
                 <label for="files" class="btn border form-control"><img src="{{asset('assets/icons/dropbox.svg')}}" alt="" srcset=""> Dropbox</label>
                 <input id="files" class="form-control" style="visibility:hidden;" type="file">
             </div>
-            <div class="w-25 m-auto col-sm-12">
+            <div class=" m-auto col-sm-12 col-md-6 col-lg-3">
                 <label for="files" class="btn border form-control"><img src="{{asset('assets/icons/drive.svg')}}" alt="" srcset=""> Google Drive</label>
                 <input id="files" class="form-control" style="visibility:hidden;" type="file">
             </div>
