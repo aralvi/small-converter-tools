ba06121249c0b745b2bef2ba8553e6a2089edce7
unknown <aralvi143@gmail.com>
2022-08-19 12:38:41 +0500

    authentication-started

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('tool/{name}',function ($name)
 {
     return view('upload-file',compact('name'));
 })->name('tools');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
